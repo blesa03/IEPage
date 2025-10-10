32f6b2494e4d525ba8a41e1edaa66cbcd7ee2fe7
Blesass <mblesapardo1@gmail.com>
2025-10-10 19:08:45 +0200

    pruebas

--- back/users/models.py
+++ back/users/models.py
@@ -15,7 +15,7 @@ class DraftUser(models.Model):
     username = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     draft = models.ForeignKey(Draft, on_delete=models.CASCADE)
-    order = models.IntegerField()
+    order = models.IntegerField(null=True, blank=True)
     
     def __str__(self):
         return self.username
