00cfbe19a0482ae751a7ab8ff8216818b2ac63d0
varo12ff <varo12ff@gmail.com>
2025-10-12 16:58:33 +0200

    url

--- back/team/urls.py
+++ back/team/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import my_team 
 
 urlpatterns = [
-    path('<int:draft_user_id>/', my_team, name='my_team'),
+    path('<int:draft_id>/', my_team, name='my_team'),
 ]
