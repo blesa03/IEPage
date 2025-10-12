d98a9df84b189584f46dfea54b1528f340a7a8a6
varo12ff <varo12ff@gmail.com>
2025-10-12 16:40:04 +0200

    fix view ranking

--- back/ranking/views.py
+++ back/ranking/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import JsonResponse, HttpRequest
 from team.models import Team
-from team.models import Team
+from draft.models import Draft
 
 
 def view_clasification(request: HttpRequest, league_id):
     if request.method != 'GET':
         return JsonResponse({'error': 'Método no permitido'}, status=405)
     
-    teams = list(Team.objects.filter(league=league_id).values().order_by('-points'))
+    draft = Draft.objects.get(league=league_id)
     
-    return JsonResponse(teams, safe=False)
+    teams = list(Team.objects.filter(draft=draft).values().order_by('-points'))
+    
+    response = [
+        {
+            'id': team['id'],
+            'points': team['points'],
+            'name': team['name']
+        } for team in teams
+    ]
+    
+    return JsonResponse(response, safe=False)
