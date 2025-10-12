7ed4a9d418df9d1854820bfd09015925af1bf1b9
varo12ff <varo12ff@gmail.com>
2025-10-12 16:54:00 +0200

    ajuste en my_team

--- back/team/views.py
+++ back/team/views.py
@@ -8,12 +8,12 @@ from players.models import DraftPlayer
 from team.models import Team
 
 
-def my_team(request: HttpRequest, draft_user_id):
+def my_team(request: HttpRequest, draft_id):
     if request.method != 'GET':
         return JsonResponse({'error': 'Método no permitido'}, status=405)
     
     try:
-        draft_user = DraftUser.objects.get(id=draft_user_id)
+        draft_user = DraftUser.objects.get(draft=draft_id, user=request.user)
     except Draft.DoesNotExist:
         return JsonResponse({'error': 'DraftUser no encontrado'}, status=404)
     
