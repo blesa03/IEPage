1a66854d346d068f70b32e6f6814b8bd13f9a752
Blesass <mblesapardo1@gmail.com>
2025-10-12 18:53:32 +0200

    gunicorn + envs

--- back/core/settings.py
+++ back/core/settings.py
@@ -29,10 +29,11 @@ MEDIA_ROOT = BASE_DIR.parent / "front" / "public"
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-8tr6twapj+f#!mf&kx3gpczetl2&rkk*z43xv6gu5_llh!c%if'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# --- Producción básica ---
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# Permite que responda en Render u otros hosts
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
@@ -59,6 +60,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -66,6 +68,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'core.urls'
 
@@ -138,7 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -146,17 +151,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    os.getenv("FRONT_ORIGIN", "http://localhost:5173"),
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    os.getenv("FRONT_ORIGIN", "http://localhost:5173"),
 ]
 
 # Cookies de sesión para desarrollo
