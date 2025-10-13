d27870c1a138d8004805da006e10530ea99ecfd9
varo12ff <varo12ff@gmail.com>
2025-10-13 20:15:29 +0200

    cambio de contraseña manu

--- back/users/management/commands/load_bootstrap_league.py
+++ back/users/management/commands/load_bootstrap_league.py
@@ -32,7 +32,7 @@ class Command(BaseCommand):
         {"username": "Daniwellingssss", "team_name": "Manchester Dani",     "password": "]q&U376]!{jx"},
         {"username": "Franchesco",      "team_name": "Los barbaros",        "password": "ORo29!T]7iCs"},
         {"username": "Sorey",           "team_name": "TIRES",               "password": "8[o%CvB86riK"},
-        {"username": "lojnoe",          "team_name": "Barrio Alto",        "password": "8[o%CvB26riK"},
+        {"username": "lojnoe",          "team_name": "Barrio Alto",        "password": "1234"},
     ]
 
     def add_arguments(self, parser):
