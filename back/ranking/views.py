4e1b7e88b518e2c88da33eb25187c01e5cf96f98
varo12ff <varo12ff@gmail.com>
2025-10-25 16:54:22 +0200

    ranking de goleadores y porteros

--- back/ranking/views.py
+++ back/ranking/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpRequest
 from team.models import Team
 from draft.models import Draft
+from games.models import Stats
+from django.db.models import Sum
+from players.types import PlayerPosition
 
 
 def view_clasification(request: HttpRequest, league_id):
@@ -21,3 +24,36 @@ def view_clasification(request: HttpRequest, league_id):
     ]
     
     return JsonResponse(response, safe=False)
+
+def get_scorers(request: HttpRequest, league_id):
+    if request.method != 'GET':
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
+    
+    draft = Draft.objects.get(league=league_id)
+    
+    top_scorers = (
+        Stats.objects
+        .filter(draft_player__draft=draft)
+        .values('draft_player__id', 'draft_player__name')
+        .annotate(total_goals=Sum('goals'))
+        .order_by('-total_goals')[:10]
+    )
+    
+    return JsonResponse(list(top_scorers), safe=False)
+
+
+def get_top_goalkeepers(request: HttpRequest, league_id):
+    if request.method != 'GET':
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
+    
+    draft = Draft.objects.get(league=league_id)
+    
+    top_goalkeepers = (
+        Stats.objects
+        .filter(draft_player__player__position=PlayerPosition.GOALKEEPER, draft_player__draft=draft)
+        .values('draft_player__id', 'draft_player__name')
+        .annotate(total_goals=Sum('goals_against'))
+        .order_by('total_goals')[:10]
+    )
+    
+    return JsonResponse(list(top_goalkeepers), safe=False)
